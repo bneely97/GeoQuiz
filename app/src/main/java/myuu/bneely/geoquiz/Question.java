17be61bf0ec40dc7b94fd9254c099bd6f8d0586b
student <student@WS-C07S300KG1J0.local>
2018-10-04 10:15:56 -0500

    Initial commit (end of chapter 3)

---
+++ app/src/main/java/myuu/bneely/geoquiz/Question.java
@@ -0,0 +1,28 @@
+package myuu.bneely.geoquiz;
+
+public class Question {
+
+        private int mTextResId; //int is integer//
+        private boolean mAnswerTrue; //boolean can mean true or false//
+
+        public Question(int textResId, boolean answerTrue) {
+            mTextResId = textResId;
+            mAnswerTrue = answerTrue;
+        }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
